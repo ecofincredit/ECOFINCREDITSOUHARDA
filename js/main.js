e4c079f122cdaf4cc2b0de643509347c5f2bfb6e
ecofincredit <ecofincredit@gmail.com>
2026-07-11 23:21:03 +0530

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        if (this.getAttribute('href') !== '#') {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+
+// Optional: Auto carousel already handled by Bootstrap
